0c2302e0bfa290e302b52c6908444300b1eac1da
wojciech_dymek <wojciechdymek94@gmail.com>
2016-05-21 18:30:43 +0200

    refactoring

--- CoreModule/COMMUNICATION_MODULE/include/LinuxServer.h
+++ CoreModule/COMMUNICATION_MODULE/include/LinuxServer.h
@@ -8,11 +8,8 @@
 #include <stdio.h>
 #include <thread>
 #include <sys/time.h>
-#include <atomic>
 #include <cerrno>
 
-#include "Structures.h"
-#include "Data.h"
 #include "AbstractServer.h"
 
 class LinuxServer : public AbstractServer
@@ -26,19 +23,10 @@ class LinuxServer : public AbstractServer
         struct sockaddr_in serverAddr;
         socklen_t addr_size;
         struct sockaddr_storage serverStorage;
-        std::atomic_bool isStopped;
         std::thread* t;
-        Data* handler;
+
 
         void startServerInNewThread();
-        void registerStructuresHandler(Data*);
-
-        void parsePoint2DSet(structDefinitions::Point2DSet*);
-        void parsePoint3DSet(structDefinitions::Point3DSet*);
-        void parseVertexSet(structDefinitions::VertexSet*);
-        void parseEdgeSet(structDefinitions::EdgeSet*);
-        void parseTriangleFaceSet(structDefinitions::TriangleFaceSet*);
-        void parseBlockSet(structDefinitions::BlockSet*);
 };
 
 #endif // SERVER_H
---
+++ CoreModule/COMMUNICATION_MODULE/src/AbstractServer.cpp
@@ -0,0 +1,94 @@
+#include "AbstractServer.h"
+
+void AbstractServer::registerStructuresHandler(Data* data) {
+    if (this->handler != NULL) {
+        this->handler = data;
+    }
+}
+
+Point3D AbstractServer::parsePoint(const structDefinitions::Point3D* p) {
+    return Point3D(p->x(), p->y(), p->z());
+}
+
+void AbstractServer::parseBlockSet(structDefinitions::BlockSet* blockSet) {
+    for(int i=0; i<blockSet->blocks_size(); i++) {
+        const structDefinitions::Block b = blockSet->blocks(i);
+        vector<Point3D> points;
+        vector<Face> faces;
+
+        points.push_back(parsePoint(&b.v1()));
+        points.push_back(parsePoint(&b.v2()));
+        points.push_back(parsePoint(&b.v3()));
+        points.push_back(parsePoint(&b.v4()));
+
+        Block* block = new Block(&points, 0, &faces);
+        this -> handler -> add(0, block);
+    }
+}
+
+void AbstractServer::parseEdgeSet(structDefinitions::EdgeSet* edgeSet) {
+    for (int i=0; i<edgeSet->edges_size(); i++) {
+        const structDefinitions::Edge e = edgeSet->edges(i);
+
+        vector<Point3D> points;
+        points.push_back(parsePoint(&e.v1()));
+        points.push_back(parsePoint(&e.v2()));
+
+        Edge* edge = new Edge(&points, 0);
+        this -> handler -> add(0, edge);
+    }
+}
+
+void AbstractServer::parsePoint2DSet(structDefinitions::Point2DSet* pointSet) {
+    for (int i=0; i<pointSet->points_size(); i++) {
+        const structDefinitions::Point2D p = pointSet->points(i);
+        Point3D point3D(p.x(), p.y(), 0);
+
+        vector<Point3D> points;
+        points.push_back(point3D);
+
+        Vertex* v = new Vertex(&points, 0);
+        this -> handler -> add(0, v);
+    }
+}
+
+void AbstractServer::parsePoint3DSet(structDefinitions::Point3DSet* pointSet) {
+    for (int i=0; i<pointSet->points_size(); i++) {
+        const structDefinitions::Point3D p = pointSet->points(i);
+
+        vector<Point3D> points;
+        points.push_back(parsePoint(&p));
+
+        Vertex* v = new Vertex(&points, 0);
+        this -> handler -> add(0, v);
+    }
+}
+
+void AbstractServer::parseTriangleFaceSet(structDefinitions::TriangleFaceSet* triangleFaceSet) {
+    for (int i=0; i<triangleFaceSet->trianglefaces_size(); i++) {
+        const structDefinitions::TriangleFace t = triangleFaceSet->trianglefaces(i);
+
+        vector<Point3D> points;
+        vector<Edge> edges;
+
+        points.push_back(parsePoint(&t.v1()));
+        points.push_back(parsePoint(&t.v2()));
+        points.push_back(parsePoint(&t.v3()));
+
+        Face* f = new Face(&points, 0, &edges);
+        this -> handler -> add(0, f);
+    }
+}
+
+void AbstractServer::parseVertexSet(structDefinitions::VertexSet* vertexSet) {
+    for (int i=0; i<vertexSet->vertexes_size(); i++) {
+        const structDefinitions::Vertex v = vertexSet->vertexes(i);
+
+        vector<Point3D> points;
+
+        points.push_back(parsePoint(&v.point()));
+
+        Vertex* vertex = new Vertex(&points, 0);
+        this->handler->add(0, vertex);
+    }
+}
