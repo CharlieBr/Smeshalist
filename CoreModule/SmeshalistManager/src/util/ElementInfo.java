a8df5b97f4a2816d73076794a73b4c6a5aaa2199
Katarzyna <kmakonieczna@gmail.com>
2016-06-04 21:07:45 +0200

    statistics tab created

---
+++ CoreModule/SmeshalistManager/src/util/ElementInfo.java
@@ -0,0 +1,38 @@
+package util;
+
+public class ElementInfo {
+	private Integer total;
+	private Integer visible;
+	
+	
+	public ElementInfo(Integer total, Integer visible) {
+
+		this.total = total;
+		this.visible = visible;
+	}
+
+
+	public Integer getTotal() {
+		return total;
+	}
+
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+
+	public Integer getVisible() {
+		return visible;
+	}
+
+
+	public void setVisible(Integer visible) {
+		this.visible = visible;
+	}
+	
+	
+	
+	
+
+}
--- CoreModule/SmeshalistManager/src/window/MainWindow.java
+++ CoreModule/SmeshalistManager/src/window/MainWindow.java
@@ -8,17 +8,18 @@ public class MainWindow extends JFrame{
 	
 	private static final long serialVersionUID = -8385942516800601932L;
 	JTabbedPane tabContainer;
-	JPanel statisticsTab;
-	JPanel optionsTab;
-	JPanel filtersTab;
+	StatisticsTab statisticsTab;
+	OptionsTab optionsTab;
+	FiltersTab filtersTab;
 		
 	public MainWindow(){
 		this.setSize(500,500);
-		setVisible(true);
+
 		tabContainer = new JTabbedPane();
 		tabContainer.setVisible(true);
 		this.optionsTab = new OptionsTab();
 		this.statisticsTab = new StatisticsTab();
+		statisticsTab.setTestContent();
 		this.filtersTab = new FiltersTab();
 
 		tabContainer.addTab("Statistics", statisticsTab);
@@ -26,6 +27,7 @@ public class MainWindow extends JFrame{
 		tabContainer.addTab("Filters", filtersTab);
 		this.add(tabContainer);
 
+		setVisible(true);
 	}
 
 	public static void main(String[] args){
