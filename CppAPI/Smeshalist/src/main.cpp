96ee7f5d14edbeed47f1a97510a767dd1d731ae0
Katarzyna <kmakonieczna@gmail.com>
2016-10-23 12:51:37 +0200

    test method udpate

--- CppAPI/Smeshalist/src/main.cpp
+++ CppAPI/Smeshalist/src/main.cpp
@@ -7,9 +7,12 @@
 
 using namespace std;
 
-int N = 1000;
+int N = 100;
 double frand(){
 	return ((double)((double)rand()/(double)RAND_MAX));
+}
+Point3D genPoint() {
+	return Point3D(frand()*3.0, frand()*3.0,frand()*3.0);
 }
 int main() {
 
@@ -21,6 +24,9 @@ int main() {
 	Point2D points2D[N];
 	Point3D points3D[N];
 	Vertex vertexes[N];
+	Edge edges[N];
+	Face faces[N];
+	Block blocks[N];
 
 	for (int i = 0; i < N; i++){
 		points2D[i] = Point2D(frand()*3.0, frand()*3.0);
@@ -38,6 +44,24 @@ int main() {
 		vertexes[i] = Point3D(frand()*3.0, frand()*3.0,frand()*3.0);
 		vertexes[i].SetGroupId(2);
 		tool.AddGeometry(vertexes[i]);
+	}
+
+	for (int i = 0; i < N; i++){
+		edges[i] = Edge(genPoint(),genPoint());
+		edges[i].SetGroupId(3);
+		tool.AddGeometry(edges[i]);
+	}
+
+	for (int i = 0; i < N; i++){
+		faces[i] = Face(genPoint(),genPoint(), genPoint());
+		faces[i].SetGroupId(4);
+		tool.AddGeometry(faces[i]);
+	}
+
+	for (int i = 0; i < N; i++){
+		blocks[i] = Block(genPoint(),genPoint(), genPoint(), genPoint());
+		blocks[i].SetGroupId(5);
+		tool.AddGeometry(blocks[i]);
 	}
 
     tool.FlushBuffer();
