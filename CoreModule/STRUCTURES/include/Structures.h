63e9ef81e7ffe7b21ab4e2a8bf36f4f2074f56db
CharlieBr <kasia_glab@wp.pl>
2016-11-23 00:21:37 +0100

    Update tests after changes

--- CoreModule/STRUCTURES/include/Structures.h
+++ CoreModule/STRUCTURES/include/Structures.h
@@ -4,6 +4,8 @@
 #include "Element.h"
 #include <GL/glut.h>
 
+extern bool transparentStructures;
+
 // -----------------------------------------------
 // ----------- mesh elements ---------------------
 // -----------------------------------------------
